7d9a8e2ff3c92ea5cb2185ee7bdd9471dfc8ae40
Andrei Elovikov <andrei.elovikov@intel.com>
2024-10-24 13:25:58 -0700

    latency_control/properties.hpp

--- sycl/include/sycl/ext/oneapi/latency_control/properties.hpp
+++ sycl/include/sycl/ext/oneapi/latency_control/properties.hpp
@@ -24,47 +24,31 @@ enum class latency_control_type {
   min
 };
 
-struct latency_anchor_id_key
-    : oneapi::experimental::detail::compile_time_property_key<
-          oneapi::experimental::detail::PropKind::LatencyAnchorID> {
-  template <int Anchor>
-  using value_t =
-      oneapi::experimental::property_value<latency_anchor_id_key,
-                                           std::integral_constant<int, Anchor>>;
-};
-
-struct latency_constraint_key
-    : oneapi::experimental::detail::compile_time_property_key<
-          oneapi::experimental::detail::PropKind::LatencyConstraint> {
-  template <int Target, latency_control_type Type, int Cycle>
-  using value_t = oneapi::experimental::property_value<
-      latency_constraint_key, std::integral_constant<int, Target>,
-      std::integral_constant<latency_control_type, Type>,
-      std::integral_constant<int, Cycle>>;
-};
-
 template <int Anchor>
-inline constexpr latency_anchor_id_key::value_t<Anchor> latency_anchor_id;
+struct latency_anchor_id_property
+    : oneapi::experimental::new_properties::detail::property_base<
+          latency_anchor_id_property<Anchor>, struct latency_anchor_id_key> {
+  static constexpr std::string_view property_name{
+      "sycl::ext::intel::experimental::latency_anchor_id_property"};
+
+  static constexpr auto value = latency_control;
+};
+template <int Anchor>
+inline constexpr latency_anchor_id_property<Anchor> latency_anchor_id;
+
 template <int Target, latency_control_type Type, int Cycle>
-inline constexpr latency_constraint_key::value_t<Target, Type, Cycle>
-    latency_constraint;
+struct latency_constraint_property
+    : oneapi::experimental::new_properties::detail::property_base<
+          latency_constraint_property<Target, Type, Cycle>,
+          struct latency_constraint_key> {
+  static constexpr std::string_view property_name{
+      "sycl::ext::intel::experimental::latency_constraint_property"};
 
-} // namespace ext::intel::experimental
-
-namespace ext::oneapi::experimental {
-
-template <int Target, intel::experimental::latency_control_type Type, int Cycle>
-struct property_value<
-    intel::experimental::latency_constraint_key,
-    std::integral_constant<int, Target>,
-    std::integral_constant<intel::experimental::latency_control_type, Type>,
-    std::integral_constant<int, Cycle>> {
-  using key_t = intel::experimental::latency_constraint_key;
   static constexpr int target = Target;
   static constexpr intel::experimental::latency_control_type type = Type;
   static constexpr int cycle = Cycle;
 };
 
-} // namespace ext::oneapi::experimental
+} // namespace ext::intel::experimental
 } // namespace _V1
 } // namespace sycl
