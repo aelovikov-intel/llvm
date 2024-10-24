6cae739639a08a8809ef2f9cb76154684d37fab5
Andrei Elovikov <andrei.elovikov@intel.com>
2024-10-24 13:41:54 -0700

    WIP incomplete device_global/properties.hpp

--- sycl/include/sycl/ext/oneapi/device_global/properties.hpp
+++ sycl/include/sycl/ext/oneapi/device_global/properties.hpp
@@ -21,61 +21,69 @@ namespace ext::oneapi::experimental {
 
 template <typename T, typename PropertyListT> class device_global;
 
-struct device_image_scope_key
-    : detail::compile_time_property_key<detail::PropKind::DeviceImageScope> {
-  using value_t = property_value<device_image_scope_key>;
+struct device_image_scope_property
+    : new_properties::detail::property_base<device_image_scope_property> {
+  static constexpr std::string_view property_name{
+      "sycl::ext::oneapi::experimental::device_image_scope_property"};
+
+  static constexpr const char *ir_attribute_name = "sycl-device-image-scope";
+  static constexpr std::nullptr_t ir_attribute_value = nullptr;
 };
+inline constexpr device_image_scope_property device_image_scope;
 
 enum class host_access_enum : std::uint16_t { read, write, read_write, none };
+template <host_access_enum Access>
+struct host_access_property
+    : new_properties::detail::property_base<host_access_property<Access>,
+                                            struct host_access_key> {
+  static constexpr std::string_view property_name{
+      "sycl::ext::oneapi::experimental::host_access_property"};
 
-struct host_access_key
-    : detail::compile_time_property_key<detail::PropKind::HostAccess> {
-  template <host_access_enum Access>
-  using value_t =
-      property_value<host_access_key,
-                     std::integral_constant<host_access_enum, Access>>;
+  static constexpr const char *ir_attribute_name = "sycl-host-access";
+  static constexpr host_access_enum ir_attribute_value = Access;
 };
+template <host_access_enum Access>
+inline constexpr host_access_property<Access> host_access;
+
+inline constexpr auto host_access_read = host_access<host_access_enum::read>;
+inline constexpr auto host_access_write = host_access<host_access_enum::write>;
+inline constexpr auto host_access_read_write =
+    host_access<host_access_enum::read_write>;
+inline constexpr auto host_access_none = host_access<host_access_enum::none>;
 
 enum class init_mode_enum : std::uint16_t { reprogram, reset };
-
-struct init_mode_key
-    : detail::compile_time_property_key<detail::PropKind::InitMode> {
-  template <init_mode_enum Trigger>
-  using value_t =
-      property_value<init_mode_key,
-                     std::integral_constant<init_mode_enum, Trigger>>;
-};
-
-struct implement_in_csr_key
-    : detail::compile_time_property_key<detail::PropKind::ImplementInCSR> {
-  template <bool Enable>
-  using value_t =
-      property_value<implement_in_csr_key, std::bool_constant<Enable>>;
-};
-
-inline constexpr device_image_scope_key::value_t device_image_scope;
-
-template <host_access_enum Access>
-inline constexpr host_access_key::value_t<Access> host_access;
-inline constexpr host_access_key::value_t<host_access_enum::read>
-    host_access_read;
-inline constexpr host_access_key::value_t<host_access_enum::write>
-    host_access_write;
-inline constexpr host_access_key::value_t<host_access_enum::read_write>
-    host_access_read_write;
-inline constexpr host_access_key::value_t<host_access_enum::none>
-    host_access_none;
-
 template <init_mode_enum Trigger>
-inline constexpr init_mode_key::value_t<Trigger> init_mode;
-inline constexpr init_mode_key::value_t<init_mode_enum::reprogram>
-    init_mode_reprogram;
-inline constexpr init_mode_key::value_t<init_mode_enum::reset> init_mode_reset;
+struct init_mode_property
+    : new_properties::detail::property_base<init_mode_property<Trigger>,
+                                            struct init_mode_key> {
+  static constexpr std::string_view property_name{
+      "sycl::ext::oneapi::experimental::init_mode_property"};
+
+  static constexpr const char *ir_attribute_name = "sycl-init-mode";
+  static constexpr init_mode_enum ir_attribute_value = Trigger;
+};
+template <init_mode_enum Trigger>
+inline constexpr init_mode_property<Trigger> init_mode;
+
+inline constexpr auto init_mode_reprogram =
+    init_mode<init_mode_enum::reprogram>;
+inline constexpr auto init_mode_reset = init_mode<init_mode_enum::reset>;
 
 template <bool Enable>
-inline constexpr implement_in_csr_key::value_t<Enable> implement_in_csr;
-inline constexpr implement_in_csr_key::value_t<true> implement_in_csr_on;
-inline constexpr implement_in_csr_key::value_t<false> implement_in_csr_off;
+struct implement_in_csr_property
+    : new_properties::detail::property_base<implement_in_csr_property<Enable>,
+                                            struct implement_in_csr_key> {
+  static constexpr std::string_view property_name{
+      "sycl::ext::oneapi::experimental::implement_in_csr_property"};
+
+  static constexpr const char *ir_attribute_name = "sycl-implement-in-csr";
+  static constexpr bool ir_attribute_value = Enable;
+};
+template <bool Enable>
+inline constexpr implement_in_csr_property<Enable> implement_in_csr;
+
+inline constexpr auto implement_in_csr_on = implement_in_csr<true>;
+inline constexpr auto implement_in_csr_off = implement_in_csr<false>;
 
 template <typename T, typename PropertyListT>
 struct is_property_key_of<device_image_scope_key,
@@ -91,26 +99,6 @@ struct is_property_key_of<implement_in_csr_key, device_global<T, PropertyListT>>
     : std::true_type {};
 
 namespace detail {
-template <> struct PropertyMetaInfo<device_image_scope_key::value_t> {
-  static constexpr const char *name = "sycl-device-image-scope";
-  static constexpr std::nullptr_t value = nullptr;
-};
-template <host_access_enum Access>
-struct PropertyMetaInfo<host_access_key::value_t<Access>> {
-  static constexpr const char *name = "sycl-host-access";
-  static constexpr host_access_enum value = Access;
-};
-template <init_mode_enum Trigger>
-struct PropertyMetaInfo<init_mode_key::value_t<Trigger>> {
-  static constexpr const char *name = "sycl-init-mode";
-  static constexpr init_mode_enum value = Trigger;
-};
-template <bool Enable>
-struct PropertyMetaInfo<implement_in_csr_key::value_t<Enable>> {
-  static constexpr const char *name = "sycl-implement-in-csr";
-  static constexpr bool value = Enable;
-};
-
 // Filter allowing additional conditions for selecting when to include meta
 // information for properties for device_global.
 template <typename PropT, typename Properties>
